d3c3bbf4c7ec3ac225e16322f25fed1f210544d4
den1s248 <ddeniss10hi@gmail.com>
2026-04-30 16:35:33 +0300

    Update DS.h

--- DS.h
+++ DS.h
@@ -5,7 +5,7 @@
 using namespace std;
 
 void print(const std::string& text) {
-    cout << text << endl;
+    cout << text;
 }
 
 void input(string& variable, const string& prompt) {
@@ -16,4 +16,50 @@ void input(string& variable, const string& prompt) {
 void end() {
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
     cin.get();
+}
+void draw(int x, int y,char symbol) {
+    //cout << x << y << symbol;
+    int i = 1;
+    int ii = 1;
+    while (ii <= y) {
+        while (i <= x) {
+            cout << symbol;
+            i++;
+        }
+        i = 1;
+        cout << "\n";
+        ii++;
+    }
+    ii = 0;
+    i = 0;
+}
+void draw2(int x, int y,char symbol, int posX, int posY) {
+    //cout << x << y << symbol;
+    int i = 1;
+    int ii = 1; 
+    int iii = 1;
+    int iiiii = 1;
+    x = x + 2;
+    while (posY >= iii) {
+        print("\n");
+        iii++;
+    }
+    while (ii <= y) {
+        while (iiiii <= posX) {
+            print(" ");
+            iiiii++;
+        }
+        iiiii = 1;
+        while (i <= x) {
+            cout << symbol;
+            i++;
+        }
+        i = 1;
+        cout << "\n";
+        ii++;
+    }
+    ii = 1;
+    iii = 1;
+    iiiii = 1;
+    i = 1;
 }
