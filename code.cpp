dc6e6384cceab2036ac99c35f393e08c5d32c7e9
den1s248 <ddeniss10hi@gmail.com>
2026-04-30 16:35:08 +0300

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,9 +1,14 @@
 #include "DS.h"
 
 int main () {
-    string a;
-    input(a, "a ");
-    print("hello world");
-    print(a);
+    //string a;
+    //input(a, "a ");
+    //print("hello world");
+    //print(a);
+    //draw(5, 5, '#');
+    //print("\n");
+    //draw(7, 5);
+    draw2(5,5,'@', 5, 5);
+    
     end();
 }
